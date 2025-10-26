11d8d84e9b2ac4a285214da3d867025d1ac46722
Risky dwi erlina_E41251657 <e41251657@student.polije.ac.id>
2025-10-26 12:32:54 +0700

    Create profil.php

---
+++ Sistem_Produksi_Web/view/profil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h3>Profil Djawara Dawet</h3>
+        <table class="table table-striped table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th>Tentang Djawara Dawet.</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Nama </td>
+                    <td>Djawara dihati</td>
+                </tr>
+                <tr>
+                    <td>Nama Pemilik</td>
+                    <td>Abdul Wachid Jauhari</td>
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td>081235235786</td>
+                </tr>
+                <tr>
+                    <td>Tahun Berdiri</td>
+                    <td>2016</td>
+                </tr>
+                <tr>
+                    <td>Jam Buka</td>
+                    <td>08.00-21.00</td>
+                 </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>Jl. Kartini no. 7 Nganjuk</td>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
