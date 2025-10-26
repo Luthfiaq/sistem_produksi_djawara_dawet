710c70bc7915a6595effe7bf3beec617211ac4d8
SITI MAISYAROH_GOL B <e41251031@student.polije.ac.id>
2025-10-26 12:34:35 +0700

    Create galeri.php

---
+++ Sistem_Produksi_Web/galeri.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Galeri</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h3>Galeri Produk</h3>
+        <div class="row mt-4">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/produk1.jpg" class="card-img-top" alt="Produk 1">
+                    <div class="card-body text-center">
+                        <p class="card-text">Djawara Dawet</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/produk2.jpg" class="card-img-top" alt="Produk 2">
+                    <div class="card-body text-center">
+                        <p class="card-text">Proses membuat produk</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/produk3.jpg" class="card-img-top" alt="Produk 3">
+                    <div class="card-body text-center">
+                        <p class="card-text">Hasil Produk</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
