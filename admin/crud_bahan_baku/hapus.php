840d70d4f11d9477672c3f255f6861fb38f0676f
E41251516_Muhammad Luthfi Aqrobahu_gol B <e41251516@student.polije.ac.id>
2025-12-02 12:55:30 +0700

    push

--- admin/crud_bahan_baku/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM bahan_baku WHERE id_bahan_baku='$id'");
-echo "<script>alert('Data berhasil dihapus'); window.location='index.php';</script>";
-?>
--- admin/crud_data_produk/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM data_produk WHERE id_data_produk='$id'");
-echo "<script>alert('Data berhasil dihapus'); window.location='index.php';</script>";
-?>
